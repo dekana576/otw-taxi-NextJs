9f503d4bd8d6134998e3bc2cdc05ad53a84f7f34
dekana576 <dekana576@gmail.com>
2026-03-03 14:40:27 +0800

    Membuat Desain Layout Car Detail

--- otw-taxi/app/(home)/[car_id]/detail/page.tsx
+++ otw-taxi/app/(home)/[car_id]/detail/page.tsx
@@ -1,14 +1,62 @@
 "use client";
 
+import { Button, Card, CardBody, CardHeader, Image } from "@heroui/react";
+import { Car } from "lucide-react";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 export default function CarDetail() {
   const params = useParams();
   const id = params.car_id;
   return (
-    <div className="p-10">
-      <h1 className="text-2xl font-bold">Car Detail</h1>
-      <p className="mt-4">Car ID: {id}</p>
+    <div className="my-2 md:my-3 px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-center items-center">
+        <Image src="../carousel/image1.png" alt="" className="" />
+      </div>
+      <Card className="my-2 md:my-5">
+        <CardHeader>
+          <div className="bg-[#5F5F5F] rounded-3xl w-full h-50 md:h-75 flex items-center justify-center">
+            <Car size={200} className="text-white" />
+          </div>
+        </CardHeader>
+        <CardBody>
+          <div className="grid grid-cols-5 gap-2">
+            <div className="col-span-3">
+              <Card>
+                <CardBody className="h-75">
+                  <div className="text-center">
+                    <h1>
+                      <strong>Car</strong>
+                    </h1>
+                    <small className="mt-4">Car ID: {id}</small>
+                  </div>
+                  <div className="mt-auto grid md:grid-cols-3 gap-2">
+                    <Link href="/">
+                      <Button className="bg-white w-full border-2">
+                        <strong>Close</strong>
+                      </Button>
+                    </Link>
+                    <Button className="bg-black w-full md:col-span-2">
+                      <strong className="text-white">Booking</strong>
+                    </Button>
+                  </div>
+                </CardBody>
+              </Card>
+            </div>
+            <div className="col-span-2">
+              <Card>
+                <CardBody className="h-75">
+                    <div className="text-center">
+                    <h1>
+                      <strong>Driver</strong>
+                    </h1>
+                  </div>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
     </div>
   );
 }
